4ef0e56f1b5a48440d879c4fc0631f29eb3f96ae
Andreas Derksen <andreas.derksen2@gmail.com>
2026-05-27 16:14:48 +0200

    starship + aliases config

---
+++ aliases.zsh
@@ -0,0 +1,13 @@
+alias vim=nvim
+alias gs='git status'
+alias lg='lazygit'
+alias ld='lazydocker'
+alias src="cd ~/src"
+alias k='kubectl'
+# find files with preview
+alias ff='fzf --preview '\''bat --style=numbers --color=always {}'\'''
+# ls replacement
+alias ls='eza -lh --group-directories-first --icons=auto'
+alias lsa='ls -a'
+alias lt='eza --tree --level=2 --long --icons --git'
+alias lta='lt -a'
