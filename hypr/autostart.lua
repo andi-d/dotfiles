205c687eefda697c2b03f143d7f7c458345bfbb4
Andreas Derksen <andreas@derksen.io>
2026-07-14 16:44:17 +0200

    add hypr configs

---
+++ hypr/autostart.lua
@@ -0,0 +1,20 @@
+-------------------
+---- AUTOSTART ----
+-------------------
+
+-- See https://wiki.hypr.land/Configuring/Basics/Autostart/
+
+-- Autostart necessary processes (like notifications daemons, status bars, etc.)
+-- Or execute your favorite apps at launch like this:
+--
+-- hl.on("hyprland.start", function ()
+--   hl.exec_cmd(terminal)
+--   hl.exec_cmd("nm-applet")
+--   hl.exec_cmd("waybar & hyprpaper & firefox")
+-- end)
+hl.on("hyprland.start", function()
+	-- hl.exec_cmd(terminal)
+	hl.exec_cmd("nm-applet")
+	hl.exec_cmd("waybar & hyprpaper")
+	hl.exec_cmd("walker --gapplication-service")
+end)
