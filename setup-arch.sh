303802921eeb4f7e4c973f2238889d8dbf21cea8
Andreas Derksen <andreas@derksen.io>
2026-07-16 22:35:11 +0200

    add setup-arch.sh

---
+++ setup-arch.sh
@@ -0,0 +1,105 @@
+#!/bin/bash
+set -e
+
+command_exists() {
+  command -v "$1" &>/dev/null
+}
+
+sudo pacman -S --noconfirm --needed git base-devel
+
+if ! command_exists yay; then
+  if [ -d yay-install ]; then
+    rm -rf yay-install
+  fi
+  git clone https://aur.archlinux.org/yay.git yay-install
+  pushd yay-install
+  makepkg -si
+  if [ -d yay-install]; then
+    rm -rf yay-install
+  fi
+else
+  echo "yay already installed"
+fi
+
+# base
+sudo pacman --noconfirm --needed -S \
+  hyprland \
+  hyprlock \
+  hypridle \
+  hyprpaper \
+  greetd greetd-tuigreet \
+  waybar \
+  kitty \
+  tmux \
+  swaync \
+  qt6-wayland \
+  hyprpolkitagent \
+  brightnessctl \
+  wl-clipboard \
+  cliphist \
+  man-db \
+  grim \
+  slurp \
+  swappy \
+  thunar \
+  firefox \
+  pipewire \
+  wireplumber \
+  impala \
+  openssh \
+  wget \
+  zsh \
+  ttf-jetbrains-mono-nerd \
+  otf-font-awesome \
+  unzip
+
+# walker application launcher
+yay -S --noconfirm --needed \
+  elephant elephant-providerlist elephant-desktopapplications \
+  walker
+# yay -S --noconfirm --needed rofi-wayland
+elephant service enable
+systemctl --user start elephant.service
+
+# dev
+
+if ! command_exists nvm; then
+  echo "Installing nvm"
+  curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.5/install.sh | bash
+else
+  echo "nvm already installed"
+fi
+
+if ! command_exists node; then
+  nvm install --lts
+fi
+
+# nvim
+sudo pacman -S --noconfirm --needed \
+  ripgrep \
+  python \
+  python-pip \
+  luarocks \
+  fzf \
+  ast-grep \
+  fd
+
+if ! [ -d ~/.config/tmux/plugins/catppuccin ]; then
+  mkdir -p ~/.config/tmux/plugins/catppuccin
+  git clone -b v2.3.0 https://github.com/catppuccin/tmux.git ~/.config/tmux/plugins/catppuccin/tmux
+fi
+
+sudo pacman -S --noconfirm --needed \
+  dotnet-sdk
+
+#utils
+sudo pacman -S --noconfirm --needed \
+  lazygit \
+  lazydocker \
+  yazi \
+  eza
+
+# apps
+
+yay -S --noconfirm --needed \
+  brave-bin
